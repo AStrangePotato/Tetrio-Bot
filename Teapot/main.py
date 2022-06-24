a50d4e069ff15ed03cf7dfc11c9f0b3e9d19161e
AStrangePotato <79776986+AStrangePotato@users.noreply.github.com>
2022-06-24 07:47:57 -0700

    updated garbage and held detection, bugfixes

--- Teapot/main.py
+++ Teapot/main.py
@@ -6,6 +6,7 @@ from move import *
 import time
 import keyboard
 import random
+from visuals import draw
 
 
 time.sleep(3)
@@ -18,17 +19,6 @@ pieceRGB = {"s":(86,234,80),  #green
             "t":(187,51,203), #purple
             "i":(64,221,255)} #light blue
 
-class colors:
-    lime = '\x1b[38;2;86;234;80m'
-    red = '\x1b[38;2;255;83;115m'
-    lblue = '\x1b[38;2;64;221;255m'
-    magenta = '\x1b[38;2;187;51;203m'
-    yellow = '\x1b[38;2;255;255;56m'
-    blue = '\x1b[38;2;0;115;230m'
-    orange = '\x1b[38;2;255;167;26m'
-    reset = "\u001b[37m"
-    
-
 pieces = {
             'z':[[['z', 'z', 0], [0, 'z', 'z']], [[0, 0, 'z'], [0, 'z', 'z'], [0, 'z', 0]]],
             's':[[[0, 's', 's'], ['s', 's', 0]], [['s', 0, 0], ['s', 's', 0], [0, 's', 0]]],
@@ -39,51 +29,11 @@ pieces = {
             'o':[[['o', 'o'], ['o', 'o']]],
             None:None }
 
-boardMaster = [[0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0],
-               [0,0,0,0,0,0,0,0,0,0]]
-
 def getKey(pieceArr):
     for k, v in pieces.items():
         if pieceArr == v:
             return k
-        
-def getColor(pieceType):
-    match pieceType:
-        case 0:
-            return colors.reset
-        case 's':
-            return colors.lime
-        case 'z':
-            return colors.red
-        case 'j':
-            return colors.blue
-        case 'l':
-            return colors.orange
-        case 'o':
-            return colors.yellow
-        case 't':
-            return colors.magenta
-        case 'i':
-            return colors.lblue
-     
+            
 def shape(arr):
     return (len(arr), len(arr[0]))
 
@@ -103,8 +53,9 @@ def lowestBlocks(piece):
     return lowestBlocks
             
 def drop(piece, pos, board):
-    if board[0][pos] != 0:
-        return "Invalid drop location. Spot filled."
+
+    #if board[0][pos] != 0 or board[0][pos] is not None:
+        #return "Invalid drop location. Spot filled."
     
     altitude = 1
     lowestTiles = lowestBlocks(piece)
@@ -138,37 +89,6 @@ def drop(piece, pos, board):
         altitude += 1
     return board
 
-def draw(board):
-    print("\n")
-    n = 0
-    print("   ", end="")
-    for i in range(10):
-        print(str(i) + " ",end="")
-    print("\r")
-    for line in board:
-        if n < 10:
-            print(str(n) + "  ", end="")
-        else:
-            print(str(n) + " ", end="")
-
-        for tile in line:
-            print(getColor(tile) + str(tile) + " " +colors.reset, end="")
-        print("\r")
-
-        n += 1
-
-def lineClear(board):
-    for row in range(20):
-        cleared = True
-        for tile in range(10):
-            if board[row][tile] == 0:
-                cleared = False
-                break
-                
-        if cleared:
-            board.pop(row)
-            board.insert(0, [0,0,0,0,0,0,0,0,0,0])
-
 
 while True:
     pieceInfo = detect.pieceState()
@@ -177,7 +97,8 @@ while True:
         held = pieces[pieceInfo[0]]
         current = pieces[pieceInfo[1]]
         break
-time.sleep(0.1)
+
+time.sleep(0.05)
 firstPiece = True
 while True:
     if keyboard.is_pressed('q'):
@@ -185,14 +106,18 @@ while True:
 
     pieceInfo = detect.pieceState()
     currentString = pieceInfo[0]
+    '''
     if not firstPiece:
         current, next = pieces[pieceInfo[0]], pieces[pieceInfo[1]]
     elif firstPiece:
         firstPiece = False
-
+    '''
+    current, next = pieces[pieceInfo[0]], pieces[pieceInfo[1]]
     best = [-99999, False, 0,0] #(score, held t/f, [rotation, position])
 
     #!SIMULATE DROPS!#
+    boardMaster = detect.boardState()
+
     if current is not None:
         for rotation in range(len(current)):
             maxPos = 11 - len(current[rotation][0])
@@ -201,7 +126,7 @@ while True:
                 simulBoard = drop(current[rotation], pos, boardSnapshot)
                 score = hr.analyze(simulBoard)
 
-                if score == best[0] and random.uniform(0,1) < 0.35: #left bias
+                if score == best[0] and random.uniform(0,1) < 0.4: #left bias
                     best = [score, False, rotation, pos] 
                 if score > best[0]:
                     best = [score, False, rotation, pos]          
@@ -216,7 +141,7 @@ while True:
                 simulBoard = drop(held[rotation], pos, boardSnapshot)
                 score = hr.analyze(simulBoard)
 
-                if score == best[0] and random.uniform(0,1) < 0.3: #left bias
+                if score == best[0] and random.uniform(0,1) < 0.35: #left bias
                     best = [score, True, rotation, pos] 
                 if score > best[0]:
                     best = [score, True, rotation, pos]     
@@ -230,11 +155,8 @@ while True:
             hold()
             held, current = current, held
 
-        drop(current[bestRot], bestPos, boardMaster)
         place(bestRot, bestPos, getKey(current))
 
-        lineClear(boardMaster)
-        #print(bestRot, bestPos, currentString)
-        time.sleep(0.1)
+        time.sleep(0.04)
 
 draw(boardMaster)
